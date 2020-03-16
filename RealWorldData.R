f96b03171ce98c45761ef4a2680c549e3c6cfccc
Alexander Reisach <alexander.reisach@gmail.com>
2020-03-16 16:38:24 +0100

    RCT data seems to work well

--- RealWorldData.R
+++ RealWorldData.R
@@ -21,12 +21,10 @@ df = df_raw %>%
          interv = ifelse(interv=="Interv", 1, 0)) %>% 
   mutate(month_visit = (format(c_visitdate, "%Y-%m-01"))) %>% 
   mutate(post_intervention = ifelse(c_visitdate < as.Date("2015-09-01"), 1, 0))
-  ### FOR NOW ONLY INTERVENTION
-  # filter(interv==1)
 
+# create separate dataframes for treatment and control
 df_interv = df %>% 
   filter(interv==1)
-
 df_control = df %>% 
   filter(interv==0)
 
@@ -38,44 +36,26 @@ get_monthly_df = function(param_df, c_name){
   return(data)
 }
 
+# create separate column for treatment and control group
 monthly_interv = get_monthly_df(df_interv, "y_interv")
 monthly_control = get_monthly_df(df_control, "y_control")
 
-# feed into CausalImpact
-ggplot(monthly_control, aes(x, y)) + 
-  geom_point() + 
-  geom_smooth(method="lm", se = FALSE)
+x_rct = as.Date(monthly_interv$x)
+y_interv = monthly_interv$y_interv
+y_control = monthly_control$y_control
+ci_data <- (zoo(cbind(y_interv, y_control, x_rct), x_rct))
 
-ci_data <- (zoo(cbind(monthly_interv$y, monthly_control$y), as.Date(data$x)))
+# intervention vs control
+ggplot(ci_data) +
+  geom_line(aes(x_rct, y_control, color=0)) +
+  geom_line(aes(x_rct, y_interv, color=1))
+
+# feed into CausalImpact
 pre.period <- as.Date(c("2014-01-01", "2015-08-31"))
 post.period <- as.Date(c("2015-09-01", "2016-06-01"))
 impact = CausalImpact(ci_data, pre.period, post.period)
 plot(impact) 
-# + 
-  # scale_y_continuous(limits = c(-1, 1))
 
-
-
-
-
-
-# --------------------
-
-a = as.Date(df$c_visitdate)
-data = zoo(cbind(x_RCT, y_RCT), df$c_visitdate)
-pre.period <- as.Date(c("2014-01-01", "2015-09-01"))
-post.period <- as.Date(c("2015-09-01", "2016-06-01"))
-impact = CausalImpact(data, pre.period, post.period)
-
-set.seed(1)
-x1 <- 100 + arima.sim(model = list(ar = 0.999), n = 100)
-y <- 1.2 * x1 + rnorm(100)
-y[71:100] <- y[71:100] + 10
-data <- cbind(y, x1)
-
-time.points <- seq.Date(as.Date("2014-01-01"), by = 1, length.out = 100)
-data <- zoo(cbind(y, x1), time.points)
-head(data)
 
 # TODO: 
 # What's the difference between baseline and endline
@@ -84,6 +64,9 @@ head(data)
 
 
 ### US voter data
+# pth = paste("data/Tanzania/data/baseline/", "3ie_base_visit.dta", sep="")
+# df_raw= data.frame(read.dta(pth))
+
 
 
 
