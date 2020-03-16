bb2dae495735a7d8df7f5b2b1e3c34b681b2eb6e
Alexander Reisach <alexander.reisach@gmail.com>
2020-03-16 15:28:35 +0100

    apply causal impact to RCT data

--- RealWorldData.R
+++ RealWorldData.R
@@ -1,98 +1,91 @@
 library(tidyverse)
 library(CausalImpact)
 library(foreign)
-
-### potentially interesting data sources:
-# https://clinicaltrials.gov/
-# kaggle dataset search
-###
-
-
-# TODO: find out what Xus data really means
-# TODO: find some time series RCT data (e.g. finance, advertising)
-
-### Use Xu paper data
-load("data/Xu_paper/FLSource.RData")
-df = data.frame(F.source, F.u.source, L.source)
-xu
-
-
-### Causal Impact toy example
-set.seed(1)
-n_days = 6
-intervention = 1
-x1 <- 100 + arima.sim(model = list(ar = 0.999), n=n_days)
-y <- 1.2 * x1 + rnorm(n_days)
-y[intervention:6] <- y[intervention:6] + 10
-data <- cbind(y, x1)
-
-matplot(data, type = "l")
-
-
-pre.period = c(1, intervention)
-post.period = c(intervention+1, n_days)
-
-impact = CausalImpact(data, pre.period, post.period)
-
-plot(impact)
-
-
-# Use Breakfast RCT data
-breakfast = new.env()
-df = data.frame(load("data/breakfast/DS0001/36174-0001-Data.rda"))
-
-# Use fertilizer Data
-fertilizer = data.frame(read.dta("data/fertilizer/SAFI_main_dataset_AER.dta"))
-head(fertilizer)
-dim(fertilizer)
-
-# Use visit Data
-# visits = data.frame()
-
-# Xu paper data
-
-
-### Tanzania Data
-
-# We have the following data:
-# baseline
-# endline
-# existing patients
-# new patients
-
 library(ggplot2)
 
+### RCT data
+
+
+# check baseline data
 pth = paste("data/Tanzania/data/baseline/", "3ie_base_visit.dta", sep="")
-df = data.frame(read.dta(pth))
+df_raw= data.frame(read.dta(pth))
 
-# TODO: 
-# group and interv column are the same
-# fac_id and fac_label are the same
-# maritalstatus is same as marstat
-# What's the difference between baseline and endline
-# we probably dont need any of the other miss columns
-# Beginning of post-intervention: 1st September 2015
-
-df_handy = df %>% 
-  dplyr::select(group, c_month)
-
+# check data
 pth = paste("data/Tanzania/data/existing pts/", "3ie_exist_vis.dta", sep="")
-df = data.frame(read.dta(pth))
+df_raw = data.frame(read.dta(pth))
 write.csv(df, "existing_patients.csv")
 
+df = df_raw %>% 
+  dplyr::select(c(interv, c_visitdate, miss)) %>% 
+  mutate(c_visitdate = as.Date(c_visitdate, format="%Y-%m-%d"),
+         interv = ifelse(interv=="Interv", 1, 0)) %>% 
+  mutate(month_visit = (format(c_visitdate, "%Y-%m-01"))) %>% 
+  mutate(post_intervention = ifelse(c_visitdate < as.Date("2015-09-01"), 1, 0))
+  ### FOR NOW ONLY INTERVENTION
+  # filter(interv==1)
 
-# Intervention miss a lot more? -> (they also have more patients, I think)
-aggregate(df$miss, by=list(df$interv), FUN=sum, na.rm=TRUE)
+df_interv = df %>% 
+  filter(interv==1)
 
-# for proper plot, sum up by month
+df_control = df %>% 
+  filter(interv==0)
 
-df$plt_visit_date = as.factor(format(as.Date(df$c_visitdate, format="%Y-%m-%d"), "%Y-%m"))
-df_plt = aggregate(df$miss, by=list(df$interv, df$plt_visit_date), FUN=mean)
+# plot group averages over time
+get_monthly_df = function(param_df, c_name){
+  data = aggregate(param_df$miss, by=list(param_df$month_visit), FUN=mean) %>% 
+      transmute(!!c_name := x, x = Group.1) %>% 
+      drop_na()
+  return(data)
+}
+
+monthly_interv = get_monthly_df(df_interv, "y_interv")
+monthly_control = get_monthly_df(df_control, "y_control")
+
+# feed into CausalImpact
+ggplot(monthly_control, aes(x, y)) + 
+  geom_point() + 
+  geom_smooth(method="lm", se = FALSE)
+
+ci_data <- (zoo(cbind(monthly_interv$y, monthly_control$y), as.Date(data$x)))
+pre.period <- as.Date(c("2014-01-01", "2015-08-31"))
+post.period <- as.Date(c("2015-09-01", "2016-06-01"))
+impact = CausalImpact(ci_data, pre.period, post.period)
+plot(impact) 
+# + 
+  # scale_y_continuous(limits = c(-1, 1))
 
 
-plt = ggplot(df_plt) + 
-  geom_point(aes(x=Group.2, y=x, color=Group.1))
-plt
+
+
+
+
+# --------------------
+
+a = as.Date(df$c_visitdate)
+data = zoo(cbind(x_RCT, y_RCT), df$c_visitdate)
+pre.period <- as.Date(c("2014-01-01", "2015-09-01"))
+post.period <- as.Date(c("2015-09-01", "2016-06-01"))
+impact = CausalImpact(data, pre.period, post.period)
+
+set.seed(1)
+x1 <- 100 + arima.sim(model = list(ar = 0.999), n = 100)
+y <- 1.2 * x1 + rnorm(100)
+y[71:100] <- y[71:100] + 10
+data <- cbind(y, x1)
+
+time.points <- seq.Date(as.Date("2014-01-01"), by = 1, length.out = 100)
+data <- zoo(cbind(y, x1), time.points)
+head(data)
+
+# TODO: 
+# What's the difference between baseline and endline
+
+
+
+
+### US voter data
+
+
 
 
 
